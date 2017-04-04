b0b934766ce2f78e4c53f7ee98be5a5aea2b19e3
Shamir Ajate <shamirajate@gmail.com>
2017-04-03 23:10:44 -0400

    Start CenterLine implementation

--- assets/SS3500-psfCalculator.js
+++ assets/SS3500-psfCalculator.js
@@ -23,47 +23,53 @@ document.onkeydown = function (evt) {
 
 
 SS3500 = {
+  totalwidth: undefined,
   width: undefined,
   height: undefined,
   verticalLites: undefined,
   horizontalLites: undefined,
   widthInput: function(width) {
-      if (width == undefined || "") {  width = Number(document.getElementById("widthInput").value); }
-      return SS3500.width = width;
+      if (width == undefined || "") {  width = Number(document.getElementById("widthInput").value/SS3500.verticalLitesInput()); }
+      return this.width = width;
    },
   heightInput: function(height) {
       if (height == undefined || "") { height = Number(document.getElementById("heightInput").value); }
-      return SS3500.height = height;
+      return this.height = height;
    },
   verticalLitesInput: function(verticalLites) {
       if (verticalLites == undefined || "") { verticalLites = Number(document.getElementById("verticalLitesInput").value); }
-      return SS3500.verticalLites = verticalLites;
+      return this.verticalLites = verticalLites;
    },
   horizontalLitesInput: function(horizontalLites) {
       if (horizontalLites == undefined || "") { horizontalLites = Number(document.getElementById("horizontalLitesInput").value); }
-      return SS3500.horizontalLites = horizontalLites;
+      return this.horizontalLites = horizontalLites;
    },
+   totalwidthInput: function(totalwidth) {
+       if (totalwidth == undefined || "") {  totalwidth = Number(document.getElementById("widthInput").value); }
+       return this.totalwidth = totalwidth;
+    },
 
   MaxExceeded: function(Max_Number) {
+    let printout = "At this height the maximum width is " + Max_Number.toString();
     document.getElementById("PSF_Result_Div").classList.remove("PSF_Result_Div");
-    document.getElementById("PSF_Result_Div").classList.add("activeWarning");      // OPTION 1 - JAMB   - ALUM REINF   after 120" Height as per Page 9 of 12.
+    document.getElementById("PSF_Result_Div").classList.add("activeWarning");
     document.getElementById("warningIcon").style.display = "inline";
-    document.getElementById("PSF_Result_Div").innerHTML = "At this height the maximum width is " + Max_Number;
-      
+
+    document.getElementById("PSF_Result_Div").innerHTML = printout;
+
    },
 TotalDimension: function() {
-      return (SS3500.widthInput() + " x " + SS3500.heightInput() + " = " + (SS3500.widthInput() * SS3500.heightInput()/144).toFixed(2) + " Sqf");
+      return (this.widthInput() + " x " + this.heightInput() + " = " + (this.widthInput() * this.heightInput()/144).toFixed(2) + " Sqf");
   },
 
 RetrieveInput: function (){
-  // Declaration of Variables that will be needed to Determine Pressures
-  //Divide Total Dimensions by number of Lites
-  width = this.widthInput();
+
+  totalwidth = this.widthInput();
   height = this.heightInput();
   verticalLites = this.verticalLitesInput();
   horizontalLites = this.horizontalLitesInput();
-
-  return [width, height, verticalLites, horizontalLites];
+  width = (totalwidth/verticalLites);
+  return [width, height, verticalLites, horizontalLites, totalwidth];
  },
 
 GlassSize: function() {
@@ -107,47 +113,47 @@ Reinforcement0 : function(width, height){
           if (height <= 48)                              // At a Height of 48"
               {
                 if (width <= 96) { psf_M1 = psf_90_90_M1; }
-                else if (width > 96) { psf_M1 = this.MaxExceeded + 96 + '\"' + " Steel Reinf needed to reach 144\"";  }
+                else if (width > 96) { psf_M1 = this.MaxExceeded(96) + '\"' + " Steel Reinf needed to reach 144\"";  }
               }
                   else if (height <= 60)                  // At a Height of 60"
                     {
                         if (width <= 72) { psf_M1 = psf_90_90_M1; }
-                        else if (width > 72) { psf_M1 = this.MaxExceeded + 72 + '\"' + " Steel Reinf needed to reach 120\""; }
+                        else if (width > 72) { psf_M1 = this.MaxExceeded(72) + '\"' + " Steel Reinf needed to reach 120\""; }
                     }
                           else if (height <= 72)              // At a Height of 72"
                             {
                                 if (width <= 60) { psf_M1 = psf_90_90_M1; }
-                                else if (width > 60) { psf_M1 = this.MaxExceeded + 60 + '\"' + " Steel Reinf needed to reach 96\""; }
+                                else if (width > 60) { psf_M1 = this.MaxExceeded(60) + '\"' + " Steel Reinf needed to reach 96\""; }
                             }
                                 else if (height <= 78)              // At a Height of 78"
                                   {
                                       if (width <= 60) { psf_M1 = psf_90_90_M1; }
-                                      else if (width > 60) { psf_M1 = this.MaxExceeded + 60 + '\"' + " Steel Reinf needed to reach 92\""; }
+                                      else if (width > 60) { psf_M1 = this.MaxExceeded(60) + '\"' + " Steel Reinf needed to reach 92\""; }
                                   }
                                       else if (height <= 84)              // At a Height of 84"
                                         {
                                             if (width <= 54) { psf_M1 = psf_90_90_M1; }
-                                            else if (width > 54) { psf_M1 = this.MaxExceeded + 54 + '\"' + " Steel Reinf needed to reach 84\""; }
+                                            else if (width > 54) { psf_M1 = this.MaxExceeded(54) + '\"' + " Steel Reinf needed to reach 84\""; }
                                         }
                                             else if (height <= 90)              // At a Height of 90"
                                               {
                                                   if (width <= 42) { psf_M1 = psf_90_90_M1; }
                                                   else if (width <= 48) { psf_M1 = psf_89d4_89d4_M1; }
-                                                  else if (width > 48) { psf_M1 = this.MaxExceeded + 48 + '\"' + " Steel Reinf needed to reach 72\""; }
+                                                  else if (width > 48) { psf_M1 = this.MaxExceeded(48) + '\"' + " Steel Reinf needed to reach 72\""; }
                                               }
                                                   else if (height <= 97.75)              // At a Height of 97.75"
                                                     {
                                                         if (width <= 36) { psf_M1 = psf_90_90_M1; }
                                                         else if (width <= 42) { psf_M1 = psf_87d9_87d9_M1; }
                                                         else if (width <= 48) { psf_M1 = psf_80_80_M1; }
-                                                        else if (width > 48) { psf_M1 = this.MaxExceeded + 48 + '\"' + " Steel Reinf needed to reach 72\""; }
+                                                        else if (width > 48) { psf_M1 = this.MaxExceeded(48) + '\"' + " Steel Reinf needed to reach 72\""; }
                                                     }
                                                         else if (height <= 102)              // At a Height of 102"
                                                           {
                                                               if (width <= 30) { psf_M1 = psf_90_90_M1; }
                                                               else if (width <= 36) { psf_M1 = psf_88d3_88d3_M1; }
                                                               else if (width <= 42) { psf_M1 = psf_77d1_77d1_M1; }
-                                                              else if (width > 42) { psf_M1 = this.MaxExceeded + 42 + '\"' + " Steel Reinf needed to reach 70\"";}
+                                                              else if (width > 42) { psf_M1 = this.MaxExceeded(42) + '\"' + " Steel Reinf needed to reach 70\"";}
                                                           }
                                                               else if (height <= 108)              // At a Height of 108"
                                                                 {
@@ -155,46 +161,46 @@ Reinforcement0 : function(width, height){
                                                                     else if (width <= 30) { psf_M1 = psf_87d5_87d5_M1; }
                                                                     else if (width <= 36) { psf_M1 = psf_74_74_M1; }
                                                                     else if (width <= 42) { psf_M1 = psf_64d5_64d5_M1; }
-                                                                    else if (width > 42) { psf_M1 = this.MaxExceeded + 42 + '\"' + " Steel Reinf needed to reach 60\""; }
+                                                                    else if (width > 42) { psf_M1 = this.MaxExceeded(42) + '\"' + " Steel Reinf needed to reach 60\""; }
                                                                 }
                                                                     else if (height <= 114)              // At a Height of 114"
                                                                       {
                                                                           if (width <= 24) { psf_M1 = psf_90_90_M1; }
                                                                           else if (width <= 30) { psf_M1 = psf_74d2_74d2_M1; }
                                                                           else if (width <= 36) { psf_M1 = psf_62d6_62d6_M1; }
-                                                                          else if (width > 36) { psf_M1 = this.MaxExceeded + 36 + '\"' + " Steel Reinf needed to reach 60\""; }
+                                                                          else if (width > 36) { psf_M1 = this.MaxExceeded(36) + '\"' + " Steel Reinf needed to reach 60\""; }
                                                                       }
                                                                       else if (height <= 120)              // At a Height of 120"
                                                                         {
                                                                             if (width <= 24) { psf_M1 = psf_78d5_78d5_M1; }
                                                                             else if (width <= 30) { psf_M1 = psf_63d4_63d4_M1; }
                                                                             else if (width <= 36) { psf_M1 = psf_53d5_53d5_M1; }
-                                                                            else if (width > 36) { psf_M1 = SS3500.this.MaxExceeded(36) + '\"' + " Steel Reinf needed to reach 60\""; }
+                                                                            else if (width > 36) { psf_M1 = this.MaxExceeded(36) + '\"' + " Steel Reinf needed to reach 60\""; }
                                                                         }
                                                                         else if (height <= 126)              // At a Height of 126"
                                                                           {
                                                                               if (width <= 24) { psf_M1 = psf_67d7_67d7_M1; }
                                                                               else if (width <= 30) { psf_M1 = psf_54d6_54d6_M1; }
                                                                               else if (width <= 36) { psf_M1 = psf_46_46_M1; }
-                                                                              else if (width > 36) { psf_M1 = SS3500.this.MaxExceeded(36)+ '\"' + " Steel Reinf needed to reach 54\""; }
+                                                                              else if (width > 36) { psf_M1 = this.MaxExceeded(36)+ '\"' + " Steel Reinf needed to reach 54\""; }
                                                                           }
                                                                           else if (height <= 132)              // At a Height of 132"
                                                                             {
                                                                                 if (width <= 24) { psf_M1 = psf_58d8_58d8_M1; }
                                                                                 else if (width <= 30) { psf_M1 = psf_47d4_47d4_M1; }
-                                                                                else if (width > 30) { psf_M1 = this.MaxExceeded + 30 + '\"' + " Steel Reinf needed to reach 54\""; }
+                                                                                else if (width > 30) { psf_M1 = this.MaxExceeded(30) + '\"' + " Steel Reinf needed to reach 54\""; }
                                                                             }
                                                                             else if (height <= 138)              // At a Height of 138"
                                                                               {
                                                                                   if (width <= 24) { psf_M1 = psf_51d4_51d4_M1; }
                                                                                   else if (width <= 30) { psf_M1 = psf_41d4_41d4_M1; }
-                                                                                  else if (width > 30) { psf_M1 = this.MaxExceeded + 30 + '\"' + " Steel Reinf needed to reach 48\""; }
+                                                                                  else if (width > 30) { psf_M1 = this.MaxExceeded(30) + '\"' + " Steel Reinf needed to reach 48\""; }
                                                                               }
                                                                               else if (height <= 144)              // At a Height of 144"
                                                                                 {
                                                                                     if (width <= 24) { psf_M1 = psf_45d2_45d2_M1; }
                                                                                     else if (width <= 30) { psf_M1 = psf_36d4_36d4_M1; }
-                                                                                    else if (width > 30) { psf_M1 = this.MaxExceeded + 30 + '\"' + " Steel Reinf needed to reach 48\""; }
+                                                                                    else if (width > 30) { psf_M1 = this.MaxExceeded(30) + '\"' + " Steel Reinf needed to reach 48\""; }
                                                                                 }
                                                                                       else if (height > 144)
                                                                                       {
@@ -329,13 +335,10 @@ Reinforcement2: function(width, height){     // Steel Reinforcement
                                                                                         psf_M3 = "System has a max height of 144\""; YellowWarningCSS();
                                                                                       }
       return psf_M3;
-    },  
+    },
 
 CalculatePSF: function (width, height, verticalLites, horizontalLites) {
 
-    //if (heightInputValue == undefined || heightInputValue == "")
-    let TotalWidth = width;
-    let widthperpanel = width / verticalLites - 1.75;
     let psf_M1, psf_M2, psf_M3;
 
     /////////////// PRINT THE RESULT TO THE WINDLOAD CALCULATION ID ELEMENT  //////////////////////
@@ -345,6 +348,7 @@ CalculatePSF: function (width, height, verticalLites, horizontalLites) {
 
     console.log("====================================");
     console.log("> Frame Size: " + this.TotalDimension());
+    console.log("> CenterLine Width " + this.width)
     console.log("> Glass Size: " + this.GlassSize());     // Calculate Glass Size
     console.log("> Reinforcement 0: (No Reinf) " + this.Reinforcement0(this.width, this.height));  // Calculate PSF for No Reinforcement
     console.log("> Reinforcement 1: (Steel R ) " + this.Reinforcement2(this.width, this.height));    // Calculate PSF for Steel Reinforcement
@@ -354,4 +358,3 @@ CalculatePSF: function (width, height, verticalLites, horizontalLites) {
 
 
 }; //End of Object SS3500
-
